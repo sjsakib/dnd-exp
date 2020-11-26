facddfd88ebed02a29d45eec17467dc6e3055ad5
sjsakib <sjsakib.bd@gmail.com>
2020-11-26 20:40:31 +0600

    feat: added custom drag handle

--- src/components/Action/index.tsx
+++ src/components/Action/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import { FaHandPaper } from 'react-icons/fa';
 
 import './styles.css';
 import { Action } from '../../redux/actions';
@@ -16,14 +17,16 @@ export default function ActionTile({ action, index }: ActionTileProps) {
       draggableId={action.id}
       index={index}
     >
-      {(provided, snapshot) => {
+      {provided => {
         return (
           <div
             className='action-tile'
             {...provided.draggableProps}
-            {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
+            <div className='action-tile__anchor' {...provided.dragHandleProps}>
+              <FaHandPaper />
+            </div>
             {action.type}
           </div>
         );
